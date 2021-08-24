94d2b81134d697ac3fafa1b71b244f68871dcb19
Pookpash <co_su-wobbe@web.de>
2021-08-24 22:29:30 +0200

    pushit baby

--- eb9cfca50e38b1a97dd6e420ab6f127442117f90/js/scripts.js
+++ eb9cfca50e38b1a97dd6e420ab6f127442117f90/js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Till & Maria Hochzeit",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Apr 23, 2021 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Apr 24, 2021 05:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Kath. Kirchengemeinde St. Paulus Hervest',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "Wir können es nicht erwarten Euch zu sehen. Für generelle Fragen, bitte Markus oder Nils fragen."
         }
     });
 
@@ -212,11 +212,11 @@ $(document).ready(function () {
         e.preventDefault();
         var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Eine Sekunde!</strong> Wir speichern mal eben alles und infizieren euer Gerät :)'));
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Alter wirklich? Falscher Code.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbz14gWPk71V4gqOS6zJ_uSl_mrouqKdMrbwD7-fIp9_puA-i-zf/exec', data)
                 .done(function (data) {
@@ -230,7 +230,7 @@ $(document).ready(function () {
                 })
                 .fail(function (data) {
                     console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Serverproblem, Beschwerde bitte an Till. '));
                 });
         }
     });
@@ -241,9 +241,9 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var location = {lat: 51.671162, lng: 6.995217};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
+        zoom: 1,
         center: location,
         scrollwheel: false
     });
@@ -255,9 +255,9 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 51.671162, lng: 6.995217};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
+        zoom: 1,
         center: la_fiesta,
         scrollwheel: false
     });
